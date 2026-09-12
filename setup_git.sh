226eb78b347f63fd751a05c292c515e4a74c7cb7
ryota-komatsu <84310137+ryota-komatsu@users.noreply.github.com>
2026-09-13 07:59:23 +0900

    initial commit

---
+++ setup_git.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+git config --global user.name "ryota-komatsu"
+git config --global user.email "84310137+ryota-komatsu@users.noreply.github.com"
+git config --global init.defaultBranch main
+git config --global credential.helper store
+git config --global pull.rebase true
