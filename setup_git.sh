b1c3dfdec3dda7938fc87345de81809d3daac8e2
ryota-komatsu <84310137+ryota-komatsu@users.noreply.github.com>
2026-09-14 04:08:02 +0900

    set git editor to nano

--- setup_git.sh
+++ setup_git.sh
@@ -5,3 +5,4 @@ git config --global user.email "84310137+ryota-komatsu@users.noreply.github.com"
 git config --global init.defaultBranch main
 git config --global credential.helper store
 git config --global pull.rebase true
+git config --global core.editor "nano"
